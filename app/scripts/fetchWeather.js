12c80e8e1f6142353872085a6c71c04a3641d5f3
Paul Markette <paul@markette.net>
2017-04-12 15:51:45 -0700

    Complete sqlite3 setup, build weather table. Add write updates from api call to database.

--- app/scripts/fetchWeather.js
+++ app/scripts/fetchWeather.js
@@ -20,7 +20,22 @@ function intervalFunc () {
       max_humidity = parseInt(yesterday_json['history']['dailysummary'][0]['maxhumidity']);
       temp_yesterday = parseInt(yesterday_json['history']['dailysummary'][0]['meantempi']);
       rain_yesterday = parseFloat(yesterday_json['history']['dailysummary'][0]['precipi']);
+
+      var sqlite3 = require('sqlite3').verbose();
+      var db = new sqlite3.Database('../db/sprinkler.db');
+
+      db.serialize(function() {
+        var stmt = db.prepare('UPDATE weather SET minhumidity = (?), maxhumidity = (?), tempyesterday = (?), rainyesterday = (?)');
+            stmt.run(min_humidity, max_humidity, temp_yesterday, rain_yesterday);
+        stmt.finalize();
+        console.log('Min Humidity: ' + min_humidity);
+        console.log('Max Humidity: ' + max_humidity);
+        console.log('Temp Yesterday: ' + temp_yesterday);
+        console.log('Rain Yesterday: ' + rain_yesterday);
+      });
+      db.close();
     })
+
     fetch('http://api.wunderground.com/api/b0cb22caebaf9a0e/geolookup/conditions/q/94619.json')
     .then(function(res) {
       return res.json();
@@ -33,6 +48,23 @@ function intervalFunc () {
       humid_factor = (30 - (min_humidity + max_humidity) / 2);
       adj = (Math.min((Math.max(0, (100 + humid_factor + temp_factor + rain_factor))), 200));
       date_now = Date();
+
+      var sqlite3 = require('sqlite3').verbose();
+      var db = new sqlite3.Database('../db/sprinkler.db');
+
+      db.serialize(function() {
+        var stmt = db.prepare('UPDATE weather SET raintoday = (?), rainfactor = (?), humidityfactor = (?), tempfactor = (?), adjustment = (?), timestamp = (?), updatedelay = (?)');
+            stmt.run(rain_today, rain_factor, humid_factor, temp_factor, adj, date_now, delay);
+        stmt.finalize();
+        console.log('Rain Today: ' + rain_today);
+        console.log('Rain Factor: ' + rain_factor);
+        console.log('Humidity Factor: ' + humid_factor);
+        console.log('Temp Factor: ' + temp_factor);
+        console.log('Adjustment: ' + adj);
+        console.log('Timestamp: ' + date_now);
+        console.log('Delay: ' + delay);
+      });
+      db.close();
   });
 }
 
@@ -40,21 +72,3 @@ intervalFunc();
 
 setInterval(intervalFunc, delay);
 
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('mydb.db');
-var check;
-db.serialize(function() {
-
-  db.run("CREATE TABLE if not exists user_info (info TEXT)");
-  var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
-});
-
-db.close();
